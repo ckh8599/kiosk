a41b6c7ae77f75b2069e217f2586cab8eb231e84
ckh8599 <ckh8599@hanmail.net>
2017-10-13 08:21:41 +0900

    'test'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
@@ -13,9 +14,11 @@ import { UserComponent } from './components/user/user.component';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     NgbModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
