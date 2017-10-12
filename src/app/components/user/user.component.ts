7f2bb59e5beed7aeef0e4cd475537f490942a106
ckh8599 <ckh8599@hanmail.net>
2017-10-12 11:12:27 +0900

    'slide'

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class UserComponent implements OnInit {
 
   title = '경환';
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+  }
 
   ngOnInit() {
   }
