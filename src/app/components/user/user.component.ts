84a9e31d702ecd4e694da92f557d19da4abfa49f
ckh8599 <ckh8599@hanmail.net>
2017-10-12 15:13:39 +0900

    'temp'

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-user',
@@ -7,12 +6,12 @@ import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  title = '경환';
+  
   constructor() {
   }
 
   ngOnInit() {
   }
+  
 
 }
